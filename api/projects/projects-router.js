f803d0e350695a5844ac3823d388c63cc88b8fe3
milklot <vbiostflex@gmail.com>
2021-04-17 16:45:33 -0400

    done with endpoints of projects router,tested, all gooooooood

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -4,16 +4,95 @@ const projectsDB = require('./projects-model');
 
 const projectsRouter = express.Router();
 
-projectsRouter.get('/api/projects/', (req, res) => {
-	projectsDB.get(req)
-		.then(projects => {
-			res.status(200).res(projects)
+projectsRouter.get('/api/projects/', async (req, res) => {
+	try {
+		const projects = await projectsDB.get();
+		res.status(200).json(projects);
+	}
+	catch (error) {
+		res.status(500).json({
+			message: "cannot get projects"
 		})
-		.catch(() => {
-			res.status(500).json({
-				message: "something went wrong"
-			})
-		})
+	}
 });
+
+projectsRouter.get('/api/projects/:id', async (req, res) => {
+	try {
+		const project = await projectsDB.get(req.params.id);
+		if (!project) {
+			res.status(404).json({
+				message: "project with this id not found"
+			})
+		}
+		else {
+			res.status(200).json(project)
+		}
+	}
+	catch (error) {
+		res.status(500).json({
+			message: "cannot get project with specific id"
+		})
+	}
+})
+
+projectsRouter.post('/api/projects/', async (req, res) => {
+	if (!req.body.name || !req.body.description) {
+		return res.status(400).json({
+			message: "missing name/description fields"
+		})
+	}
+	try {
+		const newProject = await projectsDB.insert(req.body)
+		res.status(201).json(newProject)
+	}
+	catch (error) {
+		res.status(500).json({
+			message: "project cannot be added"
+		})
+	}
+})
+
+projectsRouter.put('/api/projects/:id', async (req, res) => {
+	if (!req.body.name || !req.body.description) {
+		return res.status(400).json({
+			message: "missing name/description fields"
+		})
+	}
+	try {
+		const modifiedProject = await projectsDB.update(req.params.id, req.body)
+		if (!modifiedProject) {
+			res.status(404).json({
+				message: "project with this id does not exist"
+			})
+		}
+		else {
+			res.status(200).json(modifiedProject)
+		}
+	}
+	catch(error) {
+		res.status(500).json({
+			message: "project cannot be modified"
+		})
+	}
+})
+
+projectsRouter.delete('/api/projects/:id',async (req, res) => {
+	try {
+		const deletedProject = await projectsDB.remove(req.params.id);
+		if (!deletedProject) {
+			res.status(404).json({
+				message: "project with this id does not exist"
+			})
+		}
+		else {
+			res.status(200).json(deletedProject)
+		}
+	}
+	catch(error) {
+		res.status(500).json({
+			message: "project cannot be removed"
+		})
+	}
+})
 
 module.exports = projectsRouter;
