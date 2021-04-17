b7c7bc6cac3ddb34c796942534ad115bdf5362d3
milklot <vbiostflex@gmail.com>
2021-04-17 16:04:37 -0400

    made server up and running, made initial get request

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,1 +1,19 @@
 // Write your "projects" router here!
+const express = require('express');
+const projectsDB = require('./projects-model');
+
+const projectsRouter = express.Router();
+
+projectsRouter.get('/api/projects/', (req, res) => {
+	projectsDB.get(req)
+		.then(projects => {
+			res.status(200).res(projects)
+		})
+		.catch(() => {
+			res.status(500).json({
+				message: "something went wrong"
+			})
+		})
+});
+
+module.exports = projectsRouter;
--- api/server.js
+++ api/server.js
@@ -1,7 +1,18 @@
-const express = require('express');
+const express = require('express')
+const projectsRouter = require('./projects/projects-router');
+
 const server = express();
 
 // Complete your server here!
 // Do NOT `server.listen()` inside this file!
+
+server.use(express.json());
+server.use(projectsRouter);
+
+server.get('/', (req, res) => {
+	res.status(200).json({
+		message: "server is up and running"
+	})
+});
 
 module.exports = server;
